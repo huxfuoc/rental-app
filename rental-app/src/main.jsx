2693a40dc7486eac65f533f9c8e6b9e9f15eb8ce
huxfuoc <hphc0811@gmail.com>
2024-03-14 13:56:00 +0700

    abcxyz

--- rental-app/src/main.jsx
+++ rental-app/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Layout from './layout/Layout.jsx'
 import './index.scss'
-import Login from './components/Login.jsx'
+import Login from './pages/Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Login /> */}
-    <App />
+    <Layout />
   </React.StrictMode>,
 )
